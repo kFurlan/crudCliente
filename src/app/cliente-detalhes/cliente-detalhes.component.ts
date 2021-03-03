f9205734abe8fb5eeb258747b5749d7104677795
KarinaF <kaka.furlan.kf@gmail.com>
2021-03-03 19:20:12 -0300

    cria detalhes cliente

---
+++ src/app/cliente-detalhes/cliente-detalhes.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cliente } from '../Cliente';
+import { ClienteService } from '../cliente.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-cliente-detalhes',
+  templateUrl: './cliente-detalhes.component.html',
+  styleUrls: ['./cliente-detalhes.component.css']
+})
+export class ClienteDetalhesComponent implements OnInit {
+
+  @Input() cliente: Cliente; 
+  constructor(private clienteService: ClienteService, private route: ActivatedRoute,  private location: Location) { }
+
+  ngOnInit(): void {
+    this.getCliente();
+  }
+
+  getCliente(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.clienteService.getCliente(id)
+      .subscribe(cliente => this.cliente = this.cliente);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.clienteService.updateCliente(this.cliente)
+      .subscribe(() => this.goBack());
+  }
+
+}
