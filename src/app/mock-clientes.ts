526068398f86c56e13926353e93a8f89132ef705
KarinaF <kaka.furlan.kf@gmail.com>
2021-03-03 17:49:18 -0300

    mock cliente

---
+++ src/app/mock-clientes.ts
@@ -0,0 +1,7 @@
+import { Cliente } from './Cliente';
+
+export  const clientes: Cliente[] = [
+    { id: 1, nome: "Karina", telefone: "9999999", documento: "9999999", endereco: "rua A" },
+    { id: 2, nome: "Fábio", telefone: "8888888", documento: "8888888", endereco: "rua B" },
+    { id: 3, nome: "Jorge", telefone: "7777777", documento: "7777777", endereco: "rua C" }
+  ];
